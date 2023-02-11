5c24399e8f5b4319caec21fc7b60f8f6b3b87872
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-02-11 19:58:28 +0200

    1 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  contacts = {
+    name: 'Ihor',
+    id: '1',
+  };
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+          gap: '10px',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <button type="submit">Add contact</button>
+      </div>
+    );
+  }
+}
