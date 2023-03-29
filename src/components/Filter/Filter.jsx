2f20bea16a42c1796f93d3ae04b27acfdf25d209
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-03-29 22:42:28 +0300

    final

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import css from './Filter.module.css';
 
 export function Filter({ onFilter }) {
   return (
     <>
       <p>Find contacts by names</p>
-      <input type="text" onChange={onFilter} />
+      <input className={css.inputField} type="text" onChange={onFilter} />
     </>
   );
 }
